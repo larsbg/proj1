7484f6593eab8d3f5fdf7a62e77729ab8f65e70d
larsbg <lars.b.gustavsson>
2013-07-04 13:45:55 +0200

    Initial revision.

---
+++ src/org/example/proj1/Test1.java
@@ -0,0 +1,14 @@
+package org.example.proj1;
+
+public class Test1 {
+
+    // Mod 1
+    // Mod 2
+    // Mod 3 (after tag)
+    // Mod 4
+    // Mod 5
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
---
+++ src/org/example/proj1/Test2.java
@@ -0,0 +1,11 @@
+package org.example.proj1;
+
+public class Test2 {
+
+    // Mod 1
+    // Mod 2
+    public static void main(String[] args) {
+        System.out.println("Hello World 2!");
+    }
+
+}
