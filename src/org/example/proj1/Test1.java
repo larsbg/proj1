a1e8fde4a5a694cacb7eae8cf8c33f05e6620d85
larsbg <lars.b.gustavsson>
2013-07-04 13:49:21 +0200

    Mod6

--- src/org/example/proj1/Test1.java
+++ src/org/example/proj1/Test1.java
@@ -7,6 +7,7 @@ public class Test1 {
     // Mod 3 (after tag)
     // Mod 4
     // Mod 5
+    // Mod 6
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
